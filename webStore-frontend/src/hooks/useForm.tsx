cae4c50048624f8af5a31f5ca89c281098c14eb7
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-10 19:44:54 +0300

    added packages and created login register and etc

---
+++ webStore-frontend/src/hooks/useForm.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export default function useForm(initialValue: any, submitCallBack: any) {
+    const [values, setValues] = useState(initialValue);
+
+    useEffect(() => {
+        setValues(initialValue);
+    }, [initialValue]);
+
+    const changeHandler = (event: any) => {
+        const { name, value } = event.target;
+        setValues((prevValues: any) => {
+            const keys = name.split(".");
+            if (keys.length > 1) {
+                return {
+                    ...prevValues,
+                    [keys[0]]: {
+                        ...prevValues[keys[0]],
+                        [keys[1]]: value,
+                    },
+                };
+            }
+            return {
+                ...prevValues,
+                [name]: value,
+            };
+        });
+    };
+
+    const submitHandler = (e: any) => {
+        e.preventDefault();
+        submitCallBack(values);
+    };
+
+    return { values, changeHandler, submitHandler };
+}
